053d9f1f2087cd756a6fa84bd69ce8fe2b1e8cbc
unknown <kacper.bassara@gmail.com>
2019-11-06 14:28:25 +0100

    Zajęcia

---
+++ 02-DocumentFormats/El mundo hispanohablante _1.1_ Actividad preparatoria. Presentación_files/javascript(4).php
@@ -0,0 +1,1 @@
+require(['core/first'],function(){require(['theme_amarant/bootstrap','core/log'],function(bootstrap,log){jQuery('[data-toggle="tooltip"]').tooltip();jQuery('[data-toggle="popover"]').popover()})})
